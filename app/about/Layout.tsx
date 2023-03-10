58c73e303b9a0f3c3b2f7282aacd0cc7e6706aa9
Ismail Bayoussef <bayoussefismail@gmail.com>
2023-03-10 11:04:53 +0100

    about page fix

--- app/about/Layout.tsx
+++ app/about/Layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import PageTitle from "app/components/PageTitle";
-import SlidingText from "./components/slidingtext";
 import { PageWrapper } from "../components/page-wrapper";
 import { LayoutProps } from "../interfaces/LayoutProps";
 
